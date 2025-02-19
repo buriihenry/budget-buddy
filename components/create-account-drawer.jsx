262a2496889ea5b2bedfd4a8882d05718cd0e611
buriihenry <buriihenry@gmail.com>
2025-02-19 07:36:02 +0300

    implement create account drawer and integrate with dashboard

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -1,27 +1,61 @@
 "use client";
 
 import React from 'react';
-import { Drawer } from 'vaul';
+import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle } from './ui/drawer';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { accountSchema } from '@/app/lib/schema';
+import { Input } from './ui/input';
 
-const CreateAccountDrawer = () => {
+
+const CreateAccountDrawer = ({children}) => {
+    const [open, setOpen] = useState(false);
+
+    const {register, 
+      handleSubmit, 
+      formState:{errors},
+      setValue,
+      watch,
+      reset,
+    } = useForm({
+      resolver:zodResolver(accountSchema),
+      defaultValues: {
+        name: "",
+        type: "CURRENT",
+        balance: "",
+        isDefault: false,
+      },
+    });
+
+
   return (
-    <Drawer>
-  <DrawerTrigger>Open</DrawerTrigger>
-  <DrawerContent>
-    <DrawerHeader>
-      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-      <DrawerDescription>This action cannot be undone.</DrawerDescription>
-    </DrawerHeader>
-    <DrawerFooter>
-      <Button>Submit</Button>
-      <DrawerClose>
-        <Button variant="outline">Cancel</Button>
-      </DrawerClose>
-    </DrawerFooter>
-  </DrawerContent>
-</Drawer>
+    <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger  asChild>{children}</DrawerTrigger>
+        <DrawerContent>
+            <DrawerHeader>
+            <DrawerTitle>Create New Account</DrawerTitle>
+            </DrawerHeader>
+            <div>
+                <form>
+                  <div>
+                    <label htmlFor="name" className='text-sm font-kedium'>Create New Account</label>
+                    <Input 
+                    id="name" placeholder="e.g., Main Checking"
+                    {...register("name")}
+                    />
+                    {errors.name &&(
+                      <p className='text-sm text-red-500'>{errors.name.message}</p>
+                    )}
+                  </div>
+
+
+                </form>
+            </div>
+        </DrawerContent>
+    </Drawer>
 
   )
 }
 
-export default CreateAccountDrawer
+export default CreateAccountDrawer;
