87e958cf4c716cdb991c78e76369d33071007d5e
buriihenry <buriihenry@gmail.com>
2025-04-25 11:56:28 +0300

    add account type

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { accountSchema } from '@/app/lib/schema';
 import { Input } from './ui/input';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from './ui/select';
 
 
 const CreateAccountDrawer = ({children}) => {
@@ -36,10 +37,10 @@ const CreateAccountDrawer = ({children}) => {
             <DrawerHeader>
             <DrawerTitle>Create New Account</DrawerTitle>
             </DrawerHeader>
-            <div>
-                <form>
-                  <div>
-                    <label htmlFor="name" className='text-sm font-kedium'>Create New Account</label>
+            <div className='px-4 pb-4'>
+                <form className='space-y-4'>
+                  <div className='space-y-2'>
+                    <label htmlFor="name" className='text-sm font-kedium'>Account Name</label>
                     <Input 
                     id="name" placeholder="e.g., Main Checking"
                     {...register("name")}
@@ -48,6 +49,41 @@ const CreateAccountDrawer = ({children}) => {
                       <p className='text-sm text-red-500'>{errors.name.message}</p>
                     )}
                   </div>
+                  <div className='space-y-2'>
+                    <label htmlFor="type" className='text-sm font-kedium'>Account Type</label>
+                    <Select onValueChange={(value) => setValue("type", value)} defaultValues={watch("type")}
+                    >
+                    <SelectTrigger className="type">
+                        <SelectValue placeholder="Select Type" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="CURRENT">Current</SelectItem>
+                        <SelectItem value="SAVINGs">Savings</SelectItem>
+                
+                      </SelectContent>
+                    </Select>
+
+                    {errors.type &&(
+                      <p className='text-sm text-red-500'>{errors.type.message}</p>
+                    )}
+                  </div>
+
+                  <div className='space-y-2'>
+                    <label htmlFor="balance" className='text-sm font-kedium'>Initial Balance</label>
+                    <Input 
+                    id="balance"
+                    type="number"
+                    step="0.01"
+                    placeholder="0.00" 
+                    {...register("balance")}
+                    />
+                    {errors.balance &&(
+                      <p className='text-sm text-red-500'>{errors.balance.message}</p>
+                    )}
+                  </div>
+                  
+                  
+                  
 
 
                 </form>
